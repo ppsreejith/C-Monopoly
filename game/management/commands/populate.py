7a47542358d2923f05465f9637af774dfcfa8450
ppsreejith <mailz4sreejith@gmail.com>
2014-01-12 18:06:00 +0530

    Added default data database support
    Added commands for storing and retrieving default data from a sqlite database.

--- game/management/commands/populate.py
+++ game/management/commands/populate.py
@@ -1,77 +1,45 @@
 from django.core.management.base import BaseCommand
 from game.tests import createUser, createState, createEnergyIndustry,\
     createProductIndustry, createTransport, createCalamity
+from govt.models import State
+from game.models import GlobalConstants
+from transport.models import Transport
+from industry.models import ProductIndustry
+from calamity.models import Calamity
+from energy.models import EnergyIndustry
 import random
+from subprocess import call
+from pipes import quote
+from os.path import abspath
 
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
+        
+        doRandom =  'random' in args
+        doUsers = 'users' in args
+        default = 'default' in args
+        
+        self.initVar()
+        return self.populate(doRandom, doUsers, default)
+    
+    def populate(self, doRandom, doUsers, default):
+        
+        stateValues = self.stateValues
+        productValues = self.productValues
+        medProductValues = self.medProductValues
+        bigProductValues = self.bigProductValues
+        energyValues = self.energyValues
+        medEnergyValues = self.medEnergyValues
+        bigEnergyValues = self.bigEnergyValues
+        calamityValues = self.calamityValues
+        transportValues = self.transportValues
+        coordinates = self.coordinates
+        
         nameDef = "test2609_"
-        every =  'random' in args
-        everyReal = 'real' in args
-        
-        stateValues = {
-             'population' : 90,
-             'coordx':90,
-             'coordy':23,
-             }
-        productValues = {
-            'maintenance_energy' : 2,
-            'energy_per_unit' : 0.0003,
-            'cost_price' : 0.004,
-            'initial_cost' : 12,
-            'maintenance_cost':0.5,
-            'carbon_per_unit' : 0.003,
-            }
-        medProductValues = {
-            'maintenance_energy' : 11,
-            'energy_per_unit' : 0.003,
-            'cost_price' : 0.03,
-            'initial_cost' : 105,
-            'maintenance_cost':3,
-            'carbon_per_unit' : 0.03,
-            }
-        bigProductValues = {
-            'maintenance_energy' : 94,
-            'energy_per_unit' : 0.03,
-            'cost_price' : 0.2,
-            'initial_cost' : 1200,
-            'maintenance_cost':10,
-            'carbon_per_unit' : 0.3,
-            }
-        energyValues = {
-            'initial_cost' : 20,
-            'maintenance_cost' : 0.03,
-            'carbon_per_unit' : 0.3,
-            'output' : 1,
-            }
-        medEnergyValues = {
-            'initial_cost' : 200,
-            'maintenance_cost' : 0.3,
-            'carbon_per_unit' : 3,
-            'output' : 14,
-            }
-        bigEnergyValues = {
-            'initial_cost' : 2000,
-            'maintenance_cost' : 3,
-            'carbon_per_unit' : 30,
-            'output' : 200,
-            }
-        calamityValues = {
-            'severity' : 40,
-            'probability_number' : 5,
-            }
-        transportValues = {
-            'initial_cost' : 80,
-            'stopping_cost' : 0.3,
-            'travel_rate' : 0.03,
-            'carbon_cost_rate' : 0.05,
-            'energy_rate' : 0.3,
-            }
-        coordinates = [{"coordx":72,"coordy":22},{"coordx":73,"coordy":23},{"coordx":74,"coordy":15},{"coordx":77,"coordy":13},{"coordx":77,"coordy":30},{"coordx":73,"coordy":19},{"coordx":74,"coordy":26},{"coordx":78,"coordy":30},{"coordx":80,"coordy":27},{"coordx":77,"coordy":8},{"coordx":85,"coordy":25},{"coordx":76,"coordy":31},{"coordx":81,"coordy":21},{"coordx":77,"coordy":31},{"coordx":85,"coordy":20},{"coordx":91,"coordy":24},{"coordx":94,"coordy":25},{"coordx":93,"coordy":27},{"coordx":92,"coordy":25},{"coordx":92,"coordy":23},{"coordx":78,"coordy":17},{"coordx":88,"coordy":22},{"coordx":88,"coordy":27},{"coordx":80,"coordy":13},{"coordx":85,"coordy":23},{"coordx":77,"coordy":29},{"coordx":76,"coordy":33},{"coordx":91,"coordy":26}];
-        
         profiles = []
-        if every or 'users' in args:
+        
+        if doUsers:
             limit = 100
             print "Generating %d users"%limit
             for i in range(1,limit+1):
@@ -82,23 +50,39 @@ class Command(BaseCommand):
         
         states =[]
         stateIds = []
-        sfactories = []
-        mfactories = []
-        bfactories = []
-        spowerplants = []
-        mpowerplants = []
-        bpowerplants = []
-        transports = []
-        calamities = []
+        others = []
         
-        if everyReal:
-            tempStates = self.getRealStates()
-            for i,state in enumerate(tempStates):
-                states.append(createState(**state))
-                stateIds.append(states[i].id)
-        
-        #couting on probability
-        if every or 'states' in args:
+        if default:
+            if raw_input("All existing data will be removed from the database. Proceed? ([y]/n)") == "n":
+                return
+            comm = "python " + quote(abspath("manage.py")) + " flush"
+            call( comm , shell = True)
+            tempStates = State.objects.using('default_data').all()
+            globalConstant = GlobalConstants.objects.using('default_data').all()
+            tempProducts = ProductIndustry.objects.using('default_data').all()
+            tempEnergies = EnergyIndustry.objects.using('default_data').all()
+            tempTransports = Transport.objects.using('default_data').all()
+            tempCalamities = Calamity.objects.using('default_data').all()
+            
+            for globConst in globalConstant:
+                globConst.save(using='default')
+            print "GlobalConstants created."
+            for state in tempStates:
+                state.save(using='default')
+            print "States created."
+            for product in tempProducts:
+                product.save(using='default')
+            print "ProductIndustries created."
+            for energy in tempEnergies:
+                energy.save(using='default')
+            print "EnergyIndustries created."
+            for transport in tempTransports:
+                transport.save(using='default')
+            print "Transports created."
+            for calamity in tempCalamities:
+                calamity.save(using='default')
+            print "Calamities created."
+        elif doRandom:
             stateLimit = 28
             print "Generating %d states"%stateLimit
             for i in range(0,stateLimit):
@@ -122,7 +106,7 @@ class Command(BaseCommand):
                 obj['initial_cost'] += vals[3]/10.0
                 obj['maintenance_cost'] += vals[4]/1000.0
                 obj['states'] = random.sample(stateIds,random.randint(1,10))
-                sfactories.append(createProductIndustry(**obj))
+                others.append(createProductIndustry(**obj))
             print "%d Small product industries generated"%spLimit
             mpLimit = 5
             print "Generating %d medium product industries"%mpLimit
@@ -136,7 +120,7 @@ class Command(BaseCommand):
                 obj['initial_cost'] += vals[3]
                 obj['maintenance_cost'] += vals[4]/100.0
                 obj['states'] = random.sample(stateIds,random.randint(1,10))
-                mfactories.append(createProductIndustry(**obj))
+                others.append(createProductIndustry(**obj))
             print "%d Medium product industries generated"%mpLimit
             bpLimit = 2
             print "Generating %d big product industries"%bpLimit
@@ -150,7 +134,7 @@ class Command(BaseCommand):
                 obj['initial_cost'] += vals[3]*10.0
                 obj['maintenance_cost'] += vals[4]/10.0
                 obj['states'] = random.sample(stateIds,random.randint(1,10))
-                bfactories.append(createProductIndustry(**obj))
+                others.append(createProductIndustry(**obj))
             print "%d Big product industries generated"%bpLimit
             seLimit = 6
             print "Generating %d small energy industries"%seLimit
@@ -162,7 +146,7 @@ class Command(BaseCommand):
                 obj['maintenance_cost'] += vals[1]/1000.0
                 obj['carbon_per_unit'] += vals[2]/100.0
                 obj['states'] = random.sample(stateIds,random.randint(1,10))
-                spowerplants.append(createEnergyIndustry(**obj))
+                others.append(createEnergyIndustry(**obj))
             print "%d Small energy industries generated"%seLimit
             meLimit = 3
             print "Generating %d medium energy industries"%meLimit
@@ -174,7 +158,7 @@ class Command(BaseCommand):
                 obj['maintenance_cost'] += vals[1]/100.0
                 obj['carbon_per_unit'] += vals[2]/10.0
                 obj['states'] = random.sample(stateIds,random.randint(1,10))
-                mpowerplants.append(createEnergyIndustry(**obj))
+                others.append(createEnergyIndustry(**obj))
             print "%d Medium energy industries generated"%meLimit
             beLimit = 1
             print "Generating %d big energy industry"%beLimit
@@ -186,7 +170,7 @@ class Command(BaseCommand):
                 obj['maintenance_cost'] += vals[1]/10.0
                 obj['carbon_per_unit'] += vals[2]/1.0
                 obj['states'] = random.sample(stateIds,random.randint(1,10))
-                bpowerplants.append(createEnergyIndustry(**obj))
+                others.append(createEnergyIndustry(**obj))
             print "%d Big energy industry generated"%beLimit
             tLimit = 3
             print "Generating %d transports"%tLimit
@@ -198,7 +182,7 @@ class Command(BaseCommand):
                 obj['stopping_cost'] += vals[1]/100.0
                 obj['travel_rate'] += vals[2]/1000.0
                 obj['states'] = random.sample(stateIds,random.randint(4,8))
-                transports.append(createTransport(**obj))
+                others.append(createTransport(**obj))
             print "%d Transports generated"%tLimit
             cLimit = 5
             print "Generating %d calamities"%cLimit
@@ -209,38 +193,76 @@ class Command(BaseCommand):
                 obj['severity'] += vals[0]*5-20
                 obj['probability_number'] = vals[1]
                 obj['states'] = random.sample(stateIds,random.randint(4,10))
-                calamities.append(createCalamity(**obj))
+                others.append(createCalamity(**obj))
             print "%d Calamities generated"%cLimit
-    def getRealStates(self):
-        return [{'name':'Uttar Pradesh',
-                 'population':100,
-                 'growth_rate':2.02,
-                 'coordx':80,
-                 'coordy':27,
-                 'income':12.5,
-                 'income_growth_rate':5.6,
-                 },
-                 {'name':'Maharashtra',
-                 'population':66,
-                 'growth_rate':1.6,
-                 'coordx':73,
-                 'coordy':19,
-                 'income':25,
-                 'income_growth_rate':9.3,
-                 },
-                 {'name':'Bihar',
-                 'population':52,
-                 'growth_rate':2.5,
-                 'coordx':85,
-                 'coordy':25,
-                 'income':10,
-                 'income_growth_rate':10,
-                 },
-                 {'name':'West Bengal',
-                 'population':45,
-                 'growth_rate':1.3,
-                 'coordx':88,
-                 'coordy':22,
-                 'income':20,
-                 'income_growth_rate':8,
-                 }]
+        
+        if not any([doRandom, doUsers, default]):
+            print """
+The available commands are random, users, default.
+They are used to generate users, states, transports, industries and calamities
+They are described as follows:
+random \t Generates random values for all,
+users \t Generates 100 random users,
+default \t Retrieves default set values for all,
+"""
+    
+    def initVar(self):
+        self.stateValues = {
+             'population' : 90,
+             'coordx':90,
+             'coordy':23,
+             }
+        self.productValues = {
+            'maintenance_energy' : 2,
+            'energy_per_unit' : 0.0003,
+            'cost_price' : 0.004,
+            'initial_cost' : 12,
+            'maintenance_cost':0.5,
+            'carbon_per_unit' : 0.003,
+            }
+        self.medProductValues = {
+            'maintenance_energy' : 11,
+            'energy_per_unit' : 0.003,
+            'cost_price' : 0.03,
+            'initial_cost' : 105,
+            'maintenance_cost':3,
+            'carbon_per_unit' : 0.03,
+            }
+        self.bigProductValues = {
+            'maintenance_energy' : 94,
+            'energy_per_unit' : 0.03,
+            'cost_price' : 0.2,
+            'initial_cost' : 1200,
+            'maintenance_cost':10,
+            'carbon_per_unit' : 0.3,
+            }
+        self.energyValues = {
+            'initial_cost' : 20,
+            'maintenance_cost' : 0.03,
+            'carbon_per_unit' : 0.3,
+            'output' : 1,
+            }
+        self.medEnergyValues = {
+            'initial_cost' : 200,
+            'maintenance_cost' : 0.3,
+            'carbon_per_unit' : 3,
+            'output' : 14,
+            }
+        self.bigEnergyValues = {
+            'initial_cost' : 2000,
+            'maintenance_cost' : 3,
+            'carbon_per_unit' : 30,
+            'output' : 200,
+            }
+        self.calamityValues = {
+            'severity' : 40,
+            'probability_number' : 5,
+            }
+        self.transportValues = {
+            'initial_cost' : 80,
+            'stopping_cost' : 0.3,
+            'travel_rate' : 0.03,
+            'carbon_cost_rate' : 0.05,
+            'energy_rate' : 0.3,
+            }
+        self.coordinates = [{"coordx":72,"coordy":22},{"coordx":73,"coordy":23},{"coordx":74,"coordy":15},{"coordx":77,"coordy":13},{"coordx":77,"coordy":30},{"coordx":73,"coordy":19},{"coordx":74,"coordy":26},{"coordx":78,"coordy":30},{"coordx":80,"coordy":27},{"coordx":77,"coordy":8},{"coordx":85,"coordy":25},{"coordx":76,"coordy":31},{"coordx":81,"coordy":21},{"coordx":77,"coordy":31},{"coordx":85,"coordy":20},{"coordx":91,"coordy":24},{"coordx":94,"coordy":25},{"coordx":93,"coordy":27},{"coordx":92,"coordy":25},{"coordx":92,"coordy":23},{"coordx":78,"coordy":17},{"coordx":88,"coordy":22},{"coordx":88,"coordy":27},{"coordx":80,"coordy":13},{"coordx":85,"coordy":23},{"coordx":77,"coordy":29},{"coordx":76,"coordy":33},{"coordx":91,"coordy":26}];
--- game/management/commands/remove.py
+++ game/management/commands/remove.py
@@ -9,17 +9,29 @@ from calamity.models import Calamity
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
-        nameDef = "test2609_"
         every =  'all' in args
-        if every or 'users' in args:
+        doUsers = 'users' in args
+        doStates = 'states' in args
+        
+        nameDef = "test2609_"
+        
+        if every or doUsers:
             User.objects.filter(username__startswith = nameDef ).delete()
-        if every or 'states' in args:
+            print "Removed Users."
+        if every or doStates:
             State.objects.filter(name__startswith = nameDef ).delete()
             ProductIndustry.objects.filter(name__startswith = nameDef ).delete()
-            ProductIndustry.objects.filter(name__startswith = nameDef ).delete()
-            ProductIndustry.objects.filter(name__startswith = nameDef ).delete()
-            EnergyIndustry.objects.filter(name__startswith = nameDef ).delete()
-            EnergyIndustry.objects.filter(name__startswith = nameDef ).delete()
             EnergyIndustry.objects.filter(name__startswith = nameDef ).delete()
             Transport.objects.filter(name__startswith = nameDef).delete()
             Calamity.objects.filter(name__startswith = nameDef).delete()
+            print "Removed states, industries, transports and calamities"
+        if not any([every, doUsers, doStates]):
+            print """
+The available commands are all, users, states.
+They are used to remove users, states, transports, industries and calamities starting with test2609_
+They are described as follows:
+
+all \t Removes everything
+users \t Removes users starting with test2609_
+states \t Removes states, transports, industries and calamities starting with test2609_
+"""
